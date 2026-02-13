0114db81bdad5182e1ae94030c6032b00ff029de
ShashwatSingh321 <shashwatsingh73557@gmail.com>
2026-02-14 00:23:08 +0530

    18: node execution

--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -119,3 +119,23 @@ export const useUpdateWorkflow= () => {
     })
   )
 };
+
+
+
+
+// Hook to execute a workfow
+
+export const useExecuteWorkflow = () => {
+  const trpc = useTRPC();
+
+  return useMutation(
+    trpc.workflows.execute.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Workflow "${data.name}" executed`);
+      },
+      onError: (error) => {
+        toast.error(`Failed to execute workflow: ${error.message}`);
+      },
+    }),
+  );
+};
